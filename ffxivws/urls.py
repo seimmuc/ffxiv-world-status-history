05071e05fa713d09e8faa0186366be5871c9cebc
sei <29907321+seimmuc@users.noreply.github.com>
2023-04-24 03:09:03 -0700

    add world_history view

--- ffxivws/urls.py
+++ ffxivws/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('snaps/<int:snap_id>', views.snapshot_details, name='snapshot_details'),
+    path('world/<str:world_name>', views.world_history, name='world_history'),
     path('set_timezone', views.set_timezone, name='set_timezone'),
 ]
--- ffxivws/views.py
+++ ffxivws/views.py
@@ -1,12 +1,15 @@
 import zoneinfo
+from datetime import datetime, date, time, timedelta
 from itertools import chain
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, NamedTuple, List, Iterator
 
-from django.http import HttpRequest, HttpResponseRedirect
+from django.db.models import QuerySet, Choices
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.decorators.http import require_safe, require_POST
 
-from ffxivws.models import Snapshot, WorldState, DataCenter, Region
+from ffxivws.models import Snapshot, WorldState, DataCenter, Region, World
 
 
 def index(request: HttpRequest):
@@ -21,6 +24,16 @@ regions_abr_map = {
 }
 TIMEZONES = zoneinfo.available_timezones()
 TIMEZONES.remove('localtime')
+TIMEZONES_LIST = sorted(TIMEZONES)
+# Move UTC to the front of the list
+TIMEZONES_LIST.remove('UTC')
+TIMEZONES_LIST.insert(0, 'UTC')
+STATUS_ENUM: dict[str, WorldState.Status] = {e.value: e for e in WorldState.Status}
+STATUS_ORD: dict[WorldState.Status, int] = {e: i for i, e in enumerate(WorldState.Status)}
+CLASSIFICATIONS_ENUM: dict[str, WorldState.Classification] = {e.value: e for e in WorldState.Classification}
+CLASSIFICATIONS_ORD: dict[WorldState.Classification, int] = {e: i for i, e in enumerate(WorldState.Classification)}
+CHAR_CREATION_ENUM: dict[str, WorldState.CharCreation] = {e.value: e for e in WorldState.CharCreation}
+CHAR_CREATION_ORD: dict[WorldState.CharCreation, int] = {e: i for i, e in enumerate(WorldState.CharCreation)}
 
 
 @require_safe
@@ -47,6 +60,55 @@ def snapshot_details(request: HttpRequest, snap_id: int):
 
     context = {'snapshot': s, 'regions': regions, 'regions_active': r_active}
     return render(request=request, template_name='ffxivws/snapshot.html.jinja', using='jinja', context=context)
+
+
+class WorldStateSummary(NamedTuple):
+    status: List[WorldState.Status]
+    classification: List[WorldState.Classification]
+    # noinspection SpellCheckingInspection
+    # because I don't want underscores in CSS and can't use dashes here
+    charcreate: List[WorldState.CharCreation]
+    snapshot_count: int
+
+    @classmethod
+    def from_world_state_list(cls, wsl: list[WorldState]) -> 'WorldStateSummary':
+        st = cls._sorted_enum_list((ws.status for ws in wsl), STATUS_ENUM, STATUS_ORD)
+        cl = cls._sorted_enum_list((ws.classification for ws in wsl), CLASSIFICATIONS_ENUM, CLASSIFICATIONS_ORD)
+        cc = cls._sorted_enum_list((ws.char_creation for ws in wsl), CHAR_CREATION_ENUM, CHAR_CREATION_ORD)
+        return cls(status=st, classification=cl, charcreate=cc, snapshot_count=len(wsl))
+
+    T = TypeVar('T', bound=Choices)
+
+    @staticmethod
+    def _sorted_enum_list(it: Iterator[str], enum_dict: dict[str, T], enum_ord: dict[T, int]) -> list[T]:
+        return sorted((enum_dict[s] for s in set(it)), key=lambda e: enum_ord[e])
+
+
+@require_safe
+def world_history(request: HttpRequest, world_name: str):
+    if not world_name.isalpha():
+        return HttpResponse(b'Invalid world name', status=400, content_type='text/plain')
+    try:
+        world = World.objects.get(name__iexact=world_name)
+    except World.DoesNotExist:
+        return HttpResponse(b'World not found', status=404, content_type='text/plain')
+    history_length = 7
+    today = timezone.localdate()
+    from_date = today - timedelta(days=history_length)
+    from_time = datetime.combine(from_date, time(), tzinfo=timezone.get_current_timezone())
+    states: QuerySet[WorldState] = WorldState.objects.filter(world=world, snapshot__timestamp__gte=from_time)
+    by_day: dict[date, list[WorldState]] = {}
+    for state in states:
+        _get_or_add(d=by_day, key=timezone.localdate(state.snapshot.timestamp), default_factory=list).append(state)
+    days: list[tuple[date, WorldStateSummary | None]] = []
+    cd: date = today
+    while cd >= from_date:
+        days.append((cd, WorldStateSummary.from_world_state_list(by_day[cd]) if (cd in by_day) else None))
+        cd -= timedelta(days=1)
+    context = dict(days=days, world=world, from_date=from_date, today=today, timezones=TIMEZONES_LIST,
+                   ctz=timezone.get_current_timezone_name(), saved_ctz=request.session.get('timezone'))
+    print('days', days)
+    return render(request=request, template_name='ffxivws/world.html.jinja', using='jinja', context=context)
 
 
 @require_POST
